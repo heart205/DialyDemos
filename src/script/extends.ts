019c3c86cc4fda677ad144f550e0110090f08f91
xchen <7362469@qq.com>
2022-04-22 22:08:18 +0800

    feat:add script

---
+++ src/script/extends.ts
@@ -0,0 +1,45 @@
+/**
+ * @author heart
+ * @description 继承
+ * @Date 2022-04-22
+ * @description: 静态属性和方法是可被继承的。
+ * Function.__proto__ === Function.prototype // true
+ * Function的显式原型链指向的和隐式原型链都指向的是一个 因此继承的类属性可以使用Function原型链上的属性
+ *
+ * 如果是继承的关系 则 子类的__proto__ 指向的是父类
+ * 如果是直接创建的类 则 类的__proto__ 指向的是 Function.prototype
+ */
+
+// TODO: 从原型继承的方法和 从prototype的方法那个优先级高
+// 继承的原型链的优先级更高 A的优先级更高
+
+class A {
+  static toString() {
+    return "A的toString";
+  }
+}
+
+class B extends A {
+  static toString() {
+    return "B的toString()";
+  }
+}
+
+// A.__proto__ => Function.prototype
+A.__proto__.toString = function () {
+  return "Object的toString()";
+};
+
+// A.__proto__.__proto__ = Object.prototype
+A.__proto__.__proto__.toString = function () {
+  return "Function的toString()";
+};
+
+console.log(Function.__proto__.toString());
+
+console.log(Function.toString());
+
+console.log(B.toString());
+
+export {};
+
--- src/script/symbol.ts
+++ src/script/symbol.ts
@@ -4,43 +4,102 @@
  * @Date 2022-04-21
  */
 
-const fooSymbol: any = Symbol.for('foo')
+const fooSymbol: any = Symbol.for("foo");
 
-const barSymbol: any = Symbol.for('foo')
+const barSymbol: any = Symbol.for("foo");
 
 // true
-console.log(fooSymbol === barSymbol)
+console.log(fooSymbol === barSymbol);
 
-const localSymbol: any = Symbol('foo')
-console.log(localSymbol)
+const localSymbol: any = Symbol("foo");
+console.log(localSymbol);
 
-const globalSymbol: any = Symbol.for('foo')
-console.log(globalSymbol)
+const globalSymbol: any = Symbol.for("foo");
+console.log(globalSymbol);
 
 // false
-console.log(localSymbol === globalSymbol)
+console.log(localSymbol === globalSymbol);
 
 /**
  * @description keyFor 获取符号的字符串键
  */
 
 // foo
-console.log(Symbol.keyFor(fooSymbol))
+console.log(Symbol.keyFor(fooSymbol));
 
-const undefinedSymbol = Symbol.for()
-console.log(undefinedSymbol)
+const undefinedSymbol = Symbol.for();
+console.log(undefinedSymbol);
 
 // undefined
-console.log(Symbol.keyFor(undefinedSymbol))
+console.log(Symbol.keyFor(undefinedSymbol));
+
+/**
+ * @description hasInstance
+ * @param instanceof的时候会触发类上的 [Symbol.hasInstance]
+ */
 
 class Foo {
   static [Symbol.hasInstance](foo: any) {
-    return false
+    return false;
   }
 }
-const f = new Foo()
+
+console.log(Foo.__proto__);
+
+const f = new Foo();
+
+class Foo1 extends Foo {}
+
+console.log(Foo1.__proto__ === Foo); // true
 
 // true
-console.log(Foo[Symbol.hasInstance](f))
+console.log(Foo1[Symbol.hasInstance](f));
 
-export {}
+/**
+ * Symbol.match
+ * 由String.prototype.match方法使用
+ */
+
+class match {
+  static [Symbol.match](target) {
+    return [target];
+  }
+}
+// 正则表达式的原型上面有[Stmbol.match]方法 因此是match()的有效参数
+console.log("123".match(match));
+
+/**
+ * Symbol.isConcatSpreadable 是否能用concat来平铺数组 false会将值追加到末尾
+ * 数组默认打平后在追加到末尾 类数组追加到末尾
+ */
+
+const fooArr = ["arr"];
+console.log(fooArr);
+
+const bar = ["foo"];
+bar[Symbol.isConcatSpreadable] = false;
+
+console.log(bar[Symbol.isConcatSpreadable]);
+
+console.log(fooArr.concat(bar));
+
+const set = new Set();
+
+set.add("1");
+// 不是类数组对象的对象在 Symbol.isConcatSpreadable 被设置为 true 的情况下将被忽略
+set[Symbol.isConcatSpreadable] = true;
+console.log(fooArr.concat(set));
+
+/**
+ * Symbol.iterator
+ * 返回的得是一个Generator的对象 不然 for of 会报错
+ */
+
+class iteratorClass {
+  *[Symbol.iterator]() {}
+}
+const i = new iteratorClass();
+
+console.log(i[Symbol.iterator]());
+
+export {};
